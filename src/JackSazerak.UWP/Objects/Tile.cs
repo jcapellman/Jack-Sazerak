ad1b119818b08f48adfed1d849f8daf2e7e36c3d
Jarred Capellman <jcapellman@hotmail.com>
2017-11-17 22:10:23 -0600

    Added: Tile Object

---
+++ src/JackSazerak.UWP/Objects/Tile.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JackSazerak.UWP.Objects
+{
+    public class Tile
+    {
+        public Texture2D Texture { get; set; }
+
+        public Rectangle Rect { get; set; }
+
+        public Color Color { get; set; }
+    }
+}
