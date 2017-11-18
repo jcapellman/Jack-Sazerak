a31ef10c0592d64c97cbbf52984394eb8b09c4ea
Jarred Capellman <jcapellman@hotmail.com>
2017-11-17 22:10:03 -0600

    Added: LevelContainer Object

---
+++ src/JackSazerak.UWP/Objects/LevelContainer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace JackSazerak.UWP.Objects
+{
+    public class LevelContainer
+    {
+        public List<Tile> Tiles { get; set; }
+
+        public LevelContainer()
+        {
+            Tiles = new List<Tile>();
+        }
+    }
+}
