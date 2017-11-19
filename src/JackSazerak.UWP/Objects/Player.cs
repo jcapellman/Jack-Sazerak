d702d7a884d2a297682262dcb7cb1db91a7e6404
Jarred Capellman <jcapellman@hotmail.com>
2017-11-19 09:13:30 -0600

    Added: Player Object to handle Player specific attributes down the road

---
+++ src/JackSazerak.UWP/Objects/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace JackSazerak.UWP.Objects
+{
+    public class Player : Tile
+    {
+        public Player(Tile tile)
+        {
+            Texture = tile.Texture;
+            Rect = tile.Rect;
+            Color = tile.Color;
+        }
+
+        
+    }
+}
