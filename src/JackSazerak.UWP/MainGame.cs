f02a74f293192d4ee9ccbf008abb39f9fa58b76d
Jarred Capellman <jcapellman@hotmail.com>
2017-11-19 09:11:34 -0600

    Added: Basic Keyboard Input to move the player character sprite

--- src/JackSazerak.UWP/MainGame.cs
+++ src/JackSazerak.UWP/MainGame.cs
@@ -3,6 +3,7 @@ using JackSazerak.UWP.Objects;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace JackSazerak.UWP
 {
@@ -36,6 +37,30 @@ namespace JackSazerak.UWP
 
         protected override void Update(GameTime gameTime)
         {
+            var state = Keyboard.GetState();
+
+            var pressedKeys = state.GetPressedKeys();
+
+            foreach (var keyPressed in pressedKeys)
+            {
+                switch (keyPressed)
+                {
+                    case Keys.Right:
+                        currentLevel.CurrentPlayer.UpdatePosition(10, 0);
+                        break;
+                    case Keys.Left:
+                        currentLevel.CurrentPlayer.UpdatePosition(-10, 0);
+                        break;
+                    case Keys.Up:
+                        currentLevel.CurrentPlayer.UpdatePosition(0, -10);
+                        break;
+                    case Keys.Down:
+                        currentLevel.CurrentPlayer.UpdatePosition(0, 10);
+                        break;
+
+                }
+            }
+
             base.Update(gameTime);
         }
         
@@ -47,9 +72,11 @@ namespace JackSazerak.UWP
 
             foreach (var tile in currentLevel.Tiles)
             {
-                spriteBatch.Draw(tile.Texture, tile.Rect, tile.Color);
+                tile.Render(spriteBatch);
             }
 
+            currentLevel.CurrentPlayer.Render(spriteBatch);
+            
             spriteBatch.End();
 
             base.Draw(gameTime);
