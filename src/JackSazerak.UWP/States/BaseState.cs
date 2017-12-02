797fe2e68947f50dde985b7783075e5e2d2f28c7
Jarred Capellman <jcapellman@hotmail.com>
2017-12-02 10:56:56 -0600

    Added: BaseState and LevelState classes

---
+++ src/JackSazerak.UWP/States/BaseState.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JackSazerak.UWP.States
+{
+    public abstract class BaseState
+    {
+        public abstract void Render(SpriteBatch spriteBatch);
+
+        public abstract void HandleInputs(Keys[] keysPressed);
+    }
+}
---
+++ src/JackSazerak.UWP/States/LevelState.cs
@@ -0,0 +1,57 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Managers;
+using JackSazerak.UWP.Objects;
+using JackSazerak.UWP.Objects.Containers;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JackSazerak.UWP.States
+{
+    public class LevelState : BaseState
+    {
+        private readonly LevelContainer level;
+
+        public LevelState(string levelName, GameWrapper gameWrapper)
+        {
+            level = LevelManager.LoadLevel(gameWrapper, levelName);
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            foreach (var tile in level.Tiles)
+            {
+                tile.Render(spriteBatch);
+            }
+
+            level.CurrentPlayer.Render(spriteBatch);
+
+            foreach (var text in level.TextElements)
+            {
+                text.Render(spriteBatch);
+            }
+        }
+
+        public override void HandleInputs(Keys[] keysPressed)
+        {
+            foreach (var keyPressed in keysPressed)
+            {
+                switch (keyPressed)
+                {
+                    case Keys.Right:
+                        EventManager.FireEvent(ACTION.PLAYER_MOVE_RIGHT);
+                        break;
+                    case Keys.Left:
+                        EventManager.FireEvent(ACTION.PLAYER_MOVE_LEFT);
+                        break;
+                    case Keys.Up:
+                        EventManager.FireEvent(ACTION.PLAYER_MOVE_UP);
+                        break;
+                    case Keys.Down:
+                        EventManager.FireEvent(ACTION.PLAYER_MOVE_DOWN);
+                        break;
+                }
+            }
+        }
+    }
+}
