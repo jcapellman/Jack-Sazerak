fed256582749890e5ff05a08f00dd3d8129c861b
Jarred Capellman <jcapellman@hotmail.com>
2017-12-02 13:42:04 -0600

    Added: Initial work on the MainMenuState

---
+++ src/JackSazerak.UWP/States/MainMenuState.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects;
+using JackSazerak.UWP.Objects.Containers;
+using JackSazerak.UWP.Objects.JSONObjects;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JackSazerak.UWP.States
+{
+    public class MainMenuState : BaseState
+    {
+        private readonly Tile background;
+
+        public MainMenuState(GameWrapper gameWrapper)
+        {
+            background = new Tile(new LevelTile { TextureName = "mainmenu", TileType = TILE_TYPE.BACKGROUNDS}, gameWrapper);    
+        }
+
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            background.Render(spriteBatch);
+        }
+
+        public override void HandleInputs(Keys[] keysPressed)
+        {
+            if (keysPressed.Any(a => a == Keys.Enter))
+            {
+                OnSwitchState(GAME_STATES.LEVEL);
+            }
+        }
+    }
+}
