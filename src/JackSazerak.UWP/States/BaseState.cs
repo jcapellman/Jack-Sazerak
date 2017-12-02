c2035a7937b03aa0ff783be45ec55383a4d0fecf
Jarred Capellman <jcapellman@hotmail.com>
2017-12-02 13:39:50 -0600

    Added: GameState Event support in the BaseState class

--- src/JackSazerak.UWP/States/BaseState.cs
+++ src/JackSazerak.UWP/States/BaseState.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using System;
+
+using JackSazerak.UWP.Enums;
+
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace JackSazerak.UWP.States
@@ -8,5 +12,14 @@ namespace JackSazerak.UWP.States
         public abstract void Render(SpriteBatch spriteBatch);
 
         public abstract void HandleInputs(Keys[] keysPressed);
+
+        public event EventHandler<GAME_STATES> SwitchState;
+
+        public void OnSwitchState(GAME_STATES gameState)
+        {
+            var handler = SwitchState;
+
+            SwitchState?.Invoke(this, gameState);
+        }
     }
 }
