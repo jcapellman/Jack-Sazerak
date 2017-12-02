122cc21ea37ab5d0c0a999cf232cd092f7911d95
Jarred Capellman <jcapellman@hotmail.com>
2017-12-02 13:41:42 -0600

    Added: StateManager Class to handle State Changes

---
+++ src/JackSazerak.UWP/Managers/StateManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+using JackSazerak.UWP.States;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JackSazerak.UWP.Managers
+{
+    public class StateManager
+    {
+        private BaseState currentState;
+
+        public void HandleInput(Keys[] keysPressed)
+        {
+            currentState.HandleInputs(keysPressed);
+        }
+
+        public void RenderState(SpriteBatch spriteBatch)
+        {
+            currentState.Render(spriteBatch);
+        }
+
+        public event EventHandler<GAME_STATES> SwitchState;
+
+        public void OnSwitchState(GAME_STATES gameState)
+        {
+            var handler = SwitchState;
+
+            handler?.Invoke(this, gameState);
+        }
+
+        public void SetState(GAME_STATES gameState, GameWrapper gameWrapper, object payload = null)
+        {
+            switch (gameState)
+            {
+                case GAME_STATES.MAIN_MENU:
+                    currentState = new MainMenuState(gameWrapper);                    
+                    break;
+                case GAME_STATES.LEVEL:
+                    currentState = new LevelState("E1M1", gameWrapper);
+                    break;
+            }
+
+            currentState.SwitchState += CurrentState_SwitchState;
+        }
+
+        private void CurrentState_SwitchState(object sender, GAME_STATES e)
+        {
+            OnSwitchState(e);
+        }
+    }
+}
