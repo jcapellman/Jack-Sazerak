e67be0654a6695992e9de4bccb49cc74984a5b44
Jarred Capellman <jcapellman@hotmail.com>
2017-12-03 11:32:16 -0600

    Added: BaseGameObject (WIP)

---
+++ src/JackSazerak.UWP/GameObjects/BaseGameObject.cs
@@ -0,0 +1,14 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects;
+using JackSazerak.UWP.Objects.Containers;
+using JackSazerak.UWP.Objects.JSONObjects;
+
+namespace JackSazerak.UWP.GameObjects
+{
+    public class BaseGameObject : Tile
+    {
+        public BaseGameObject(string textureName, TILE_TYPE tileType, GameWrapper wrapper) : base(new LevelTile { TextureName = textureName, TileType = tileType}, wrapper)
+        {
+        }
+    }
+}
