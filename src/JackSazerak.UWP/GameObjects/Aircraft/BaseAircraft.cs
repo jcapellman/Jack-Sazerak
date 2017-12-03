974f1e991ac1e417280ebeff5ff414b3b8d7df95
Jarred Capellman <jcapellman@hotmail.com>
2017-12-03 11:31:13 -0600

    Added: BaseAircraft class to handle Player and Enemy objects

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/BaseAircraft.cs
@@ -0,0 +1,35 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public abstract class BaseAircraft : BaseGameObject
+    {
+        protected BaseAircraft(string textureName, GameWrapper wrapper) : base(textureName, TILE_TYPE.SPRITES, wrapper)
+        {
+        }
+
+        protected abstract int AgilityHorizontal();
+
+        protected abstract int AgilityVertical();
+
+        public void Move(MOVEMENT movementOption)
+        {
+            switch (movementOption)
+            {
+                case MOVEMENT.RIGHT:
+                    UpdatePosition(AgilityHorizontal(), 0);
+                    break;
+                case MOVEMENT.LEFT:
+                    UpdatePosition(AgilityHorizontal() * -1, 0);
+                    break;
+                case MOVEMENT.UP:
+                    UpdatePosition(0, AgilityVertical() * -1);
+                    break;
+                case MOVEMENT.DOWN:
+                    UpdatePosition(0, AgilityVertical());
+                    break;
+            }
+        }
+    }
+}
