78d60a54660dda57e1e05d63a794e5d69c517fec
Jarred Capellman <jcapellman@hotmail.com>
2017-12-03 11:31:32 -0600

    Added: F45 Aircraft class (WIP)

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/F45.cs
@@ -0,0 +1,16 @@
+﻿using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class F45 : BaseAircraft
+    {
+        public F45(GameWrapper wrapper) : base("F45", wrapper)
+        {
+            
+        }
+
+        protected override int AgilityHorizontal() => 50;
+
+        protected override int AgilityVertical() => 30;
+    }
+}
