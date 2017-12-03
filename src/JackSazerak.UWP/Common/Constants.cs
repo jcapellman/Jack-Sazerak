fd7169243e5a936ed9ac1a9eb0ec25dd5d20f018
Jarred Capellman <jcapellman@hotmail.com>
2017-12-03 09:31:44 -0600

    Added: 30fps input lock

---
+++ src/JackSazerak.UWP/Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace JackSazerak.UWP.Common
+{
+    public static class Constants
+    {
+        public const int GAME_UPDATE_TICKS = 333333; // 30 FPS Inputs
+    }
+}
--- src/JackSazerak.UWP/MainGame.cs
+++ src/JackSazerak.UWP/MainGame.cs
@@ -1,4 +1,7 @@
-﻿using JackSazerak.UWP.Enums;
+﻿using System;
+
+using JackSazerak.UWP.Common;
+using JackSazerak.UWP.Enums;
 using JackSazerak.UWP.Managers;
 using JackSazerak.UWP.Objects.Containers;
 
@@ -17,10 +20,19 @@ namespace JackSazerak.UWP
 
         public MainGame()
         {
+            TargetElapsedTime = TimeSpan.FromTicks(Constants.GAME_UPDATE_TICKS);
+
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreparingDeviceSettings += Graphics_PreparingDeviceSettings;
+
             Content.RootDirectory = "Content";
 
             soundManager = new SoundManager(GameWrapper);
+        }
+
+        private void Graphics_PreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+        {
+            e.GraphicsDeviceInformation.PresentationParameters.PresentationInterval = PresentInterval.One;
         }
 
         private GameWrapper GameWrapper => new GameWrapper
@@ -53,9 +65,7 @@ namespace JackSazerak.UWP
 
         protected override void Update(GameTime gameTime)
         {
-            var state = Keyboard.GetState();
-            
-            stateManager.HandleInput(state.GetPressedKeys());
+            stateManager.HandleInput(Keyboard.GetState());
 
             base.Update(gameTime);
         }
