fff609dc6b02ac5b16050232eecf13d0aaf94337
Jarred Capellman <jcapellman@hotmail.com>
2017-12-05 19:53:03 -0600

    Added: F19 Player Aircraft

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/F19.cs
@@ -0,0 +1,15 @@
+﻿using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class F19 : BaseAircraft
+    {
+        public F19(GameWrapper wrapper) : base("F19", wrapper) { }
+
+        public override string GetName() => "F19";
+
+        protected override int AgilityHorizontal() => 80;
+
+        protected override int AgilityVertical() => 80;
+    }
+}
