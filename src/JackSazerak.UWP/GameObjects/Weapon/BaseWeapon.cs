2b954373230ae3f63ad8aeacdc7a01d23401790a
Jarred Capellman <jcapellman@hotmail.com>
2017-12-07 17:24:14 -0600

    Added: BaseWeapon Class

---
+++ src/JackSazerak.UWP/GameObjects/Weapon/BaseWeapon.cs
@@ -0,0 +1,12 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Weapon
+{
+    public abstract class BaseWeapon : BaseGameObject
+    {
+        protected BaseWeapon(string textureName, GameWrapper wrapper) : base(textureName, TILE_TYPE.SPRITES, wrapper)
+        {
+        }
+    }
+}
