b7add00822522f1ec72ba0522b4fc096fe734b03
Jarred Capellman <jcapellman@hotmail.com>
2017-12-07 22:13:29 -0600

    Added: VulkanGun Weapon Type

---
+++ src/JackSazerak.UWP/GameObjects/Weapon/VulkanGun.cs
@@ -0,0 +1,15 @@
+﻿using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Weapon
+{
+    public class VulkanGun : BaseWeapon
+    {
+        protected VulkanGun(GameWrapper wrapper) : base("VulkanGun", wrapper)
+        {
+        }
+
+        protected override int RateOfFire => 10;
+
+        protected override int Damage => 5;
+    }
+}
