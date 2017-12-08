853c44d078df80f0b12ab6e8d597d00c01947f4b
Jarred Capellman <jcapellman@hotmail.com>
2017-12-08 07:57:13 -0600

    Added: Sidewinder Weapon Class

---
+++ src/JackSazerak.UWP/GameObjects/Weapon/Sidewinder.cs
@@ -0,0 +1,15 @@
+﻿using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Weapon
+{
+    public class Sidewinder : BaseWeapon
+    {
+        protected Sidewinder(GameWrapper wrapper) : base("Sidewinder", wrapper)
+        {
+        }
+
+        protected override int RateOfFire => 2;
+
+        protected override int Damage => 30;
+    }
+}
