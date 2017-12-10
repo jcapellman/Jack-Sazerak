d863c56285677dca9306c1714bbdd91215c6d05f
Jarred Capellman <jcapellman@hotmail.com>
2017-12-10 08:40:06 -0600

    Added: XB91 Class

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/XB91.cs
@@ -0,0 +1,20 @@
+﻿using JackSazerak.UWP.GameObjects.Weapon;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class XB91 : BaseAircraft
+    {
+        protected XB91(GameWrapper wrapper) : base("xb91", new VulkanGun(wrapper), wrapper)
+        {
+        }
+        
+        public override string Name => "XB91";
+
+        protected override int AgilityHorizontal => 100;
+
+        protected override int AgilityVertical => 100;
+
+        protected override int HitPoints => 30;
+    }
+}
