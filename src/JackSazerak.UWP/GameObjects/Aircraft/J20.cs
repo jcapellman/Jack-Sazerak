e843704546327a1ba5056ad46186bfa6e9f60ac7
Jarred Capellman <jcapellman@hotmail.com>
2017-12-12 18:50:23 -0600

    Added: J20 Aircraft Class

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/J20.cs
@@ -0,0 +1,20 @@
+﻿using JackSazerak.UWP.GameObjects.Weapon;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class J20 : BaseAircraft
+    {
+        protected J20(GameWrapper wrapper) : base("J20", new VulkanGun(wrapper), wrapper)
+        {
+        }
+
+        public override string Name => "J20";
+
+        protected override int AgilityHorizontal => 40;
+
+        protected override int AgilityVertical => 50;
+
+        protected override int HitPoints => 70;
+    }
+}
