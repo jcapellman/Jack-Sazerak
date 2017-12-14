0c2b00a5f81f6e7b2c1153b7e8547f05dced2284
Jarred Capellman <jcapellman@hotmail.com>
2017-12-13 22:36:43 -0600

    Added: F28 Class

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/F28.cs
@@ -0,0 +1,20 @@
+﻿using JackSazerak.UWP.GameObjects.Weapon;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class F28 : BaseAircraft
+    {
+        protected F28(GameWrapper wrapper) : base("F28", new VulkanGun(wrapper), wrapper)
+        {
+        }
+
+        public override string Name => "F28";
+
+        protected override int AgilityHorizontal => 40;
+
+        protected override int AgilityVertical => 50;
+
+        protected override int HitPoints => 60;
+    }
+}
