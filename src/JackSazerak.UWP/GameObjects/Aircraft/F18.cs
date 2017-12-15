57ed31facebb0181ca65c64a2727cbb6d0e09dcb
Jarred Capellman <jcapellman@hotmail.com>
2017-12-15 17:06:22 -0600

    Added: F18 Class

---
+++ src/JackSazerak.UWP/GameObjects/Aircraft/F18.cs
@@ -0,0 +1,18 @@
+﻿using JackSazerak.UWP.GameObjects.Weapon;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Aircraft
+{
+    public class F18 : BaseAircraft
+    {
+        public F18(GameWrapper wrapper) : base("F18", new VulkanGun(wrapper), wrapper) { }
+
+        public override string Name => "F18";
+
+        protected override int AgilityHorizontal => 40;
+
+        protected override int AgilityVertical => 40;
+
+        protected override int HitPoints => 20;
+    }
+}
