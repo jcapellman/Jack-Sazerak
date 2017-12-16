ba411f2b5eb70aa6577d3c1eac9fb3bbbd84cc62
Jarred Capellman <jcapellman@hotmail.com>
2017-12-16 13:39:06 -0600

    Added: BackgroundTile Class Object

---
+++ src/JackSazerak.UWP/GameObjects/Static/BackgroundTile.cs
@@ -0,0 +1,17 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Static
+{
+    public class BackgroundTile : BaseGameObject
+    {
+        public BackgroundTile(string textureName, int positionX, int positionY, int width, int height, GameWrapper wrapper) : base(textureName, TILE_TYPE.TILES, wrapper)
+        {
+            Height = height;
+            Width = width;
+
+            PositionY = positionY;
+            PositionX = positionX;            
+        }
+    }
+}
