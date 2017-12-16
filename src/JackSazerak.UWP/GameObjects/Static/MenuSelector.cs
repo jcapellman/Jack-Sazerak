577879116a54fa9e7f2769411649226799b3e25b
Jarred Capellman <jcapellman@hotmail.com>
2017-12-16 13:35:27 -0600

    Added: MenuSelector Class Object

---
+++ src/JackSazerak.UWP/GameObjects/Static/MenuSelector.cs
@@ -0,0 +1,12 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.GameObjects.Static
+{
+    public class MenuSelector : BaseGameObject
+    {
+        public MenuSelector(string textureName, GameWrapper wrapper) : base(textureName, TILE_TYPE.SPRITES, wrapper)
+        {            
+        }
+    }
+}
