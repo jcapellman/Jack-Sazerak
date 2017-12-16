9ad84ff686e8114fdefbbe7a2ca70812a57a5bbc
Jarred Capellman <jcapellman@hotmail.com>
2017-12-16 14:15:09 -0600

    Change: Access to the BaseGameObject constructor and removed the LevelTile creation in the Constructor of the base class

--- src/JackSazerak.UWP/GameObjects/BaseGameObject.cs
+++ src/JackSazerak.UWP/GameObjects/BaseGameObject.cs
@@ -1,13 +1,12 @@
 ﻿using JackSazerak.UWP.Enums;
 using JackSazerak.UWP.Objects;
 using JackSazerak.UWP.Objects.Containers;
-using JackSazerak.UWP.Objects.JSONObjects;
 
 namespace JackSazerak.UWP.GameObjects
 {
-    public class BaseGameObject : Tile
+    public abstract class BaseGameObject : Tile
     {
-        public BaseGameObject(string textureName, TILE_TYPE tileType, GameWrapper wrapper) : base(new LevelTile { TextureName = textureName, TileType = tileType}, wrapper)
+        protected BaseGameObject(string textureName, TILE_TYPE tileType, GameWrapper wrapper) : base(textureName, tileType, wrapper)
         {
         }
     }
