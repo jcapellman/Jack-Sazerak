fb7b8c20df4c4721bf63411c2e1a5de0268f9f35
Jarred Capellman <jcapellman@hotmail.com>
2017-12-17 18:34:33 -0600

    Added: Initial work on Fade Transition Effect

---
+++ src/JackSazerak.UWP/Effects/Fade.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+using JackSazerak.UWP.Objects.Containers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JackSazerak.UWP.Effects
+{
+    public class Fade
+    {
+        public enum FADE_TYPE
+        {
+            FADE_IN,
+            FADE_OUT
+        };
+
+        private FADE_TYPE fadeType;
+
+        private int durationSeconds;
+        
+        private Texture2D texture;
+
+        private float changePerFrame;
+
+        private float opacity;
+
+        public event EventHandler TransitionCompleted;
+
+        public void OnTransitionCompleted()
+        {
+            TransitionCompleted?.Invoke(null, null);
+        }
+
+        public Fade(FADE_TYPE fadeType, int durationSeconds, Color color, GameWrapper gameWrapper)
+        {
+            texture = new Texture2D(gameWrapper.GraphicsDevice, gameWrapper.Window_Width, gameWrapper.Window_Height);
+
+            var data = new Color[gameWrapper.Window_Height * gameWrapper.Window_Width];
+
+            for (var i = 0; i < data.Length; ++i)
+            {
+                data[i] = color;
+            }
+
+            texture.SetData(data);
+            
+            this.fadeType = fadeType;
+            this.durationSeconds = durationSeconds;
+
+            Reset(fadeType);
+        }
+        
+        public void Reset(FADE_TYPE fadeType)
+        {
+            changePerFrame = 1.0f / (durationSeconds * 30);
+
+            switch (fadeType)
+            {
+                case FADE_TYPE.FADE_IN:
+                    opacity = 1.0f;
+                    break;
+                case FADE_TYPE.FADE_OUT:
+                    opacity = 0.0f;
+                    break;
+            }
+        }
+
+        public void Render(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Vector2(0,0), Color.White * opacity);
+
+            switch (fadeType)
+            {
+                case FADE_TYPE.FADE_IN:
+                    if (opacity <= 0.0f)
+                    {
+                        OnTransitionCompleted();
+                        break;
+                    }
+
+                    opacity -= changePerFrame;
+                    break;
+                case FADE_TYPE.FADE_OUT:
+                    if (opacity >= 1.0f)
+                    {
+                        OnTransitionCompleted();
+                        break;
+                    }
+
+                    opacity += changePerFrame;
+                    break;
+            }            
+        }
+    }
+}
