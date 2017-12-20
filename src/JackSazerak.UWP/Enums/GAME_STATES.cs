17a26c98cfd669975b96dc487274cc84a7599914
Jarred Capellman <jcapellman@hotmail.com>
2017-12-19 19:06:33 -0600

    Added: Additional States for the Main Menu

--- src/JackSazerak.UWP/Enums/GAME_STATES.cs
+++ src/JackSazerak.UWP/Enums/GAME_STATES.cs
@@ -3,6 +3,11 @@
     public enum GAME_STATES
     {
         MAIN_MENU,
+        MAIN_MENU_NEWGAME,
+        MAIN_MENU_SAVEGAME,
+        MAIN_MENU_LOADGAME,
+        MAIN_MENU_OPTIONS,
+        MAIN_MENU_ABOUT,
         LEVEL
     }
 }
