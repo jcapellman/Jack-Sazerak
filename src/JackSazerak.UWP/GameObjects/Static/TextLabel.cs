b443514ec9b818c56934c54c99dd5287ead90a97
Jarred Capellman <jcapellman@hotmail.com>
2017-12-23 08:38:46 -0600

    Added: TextLabel Object

---
+++ src/JackSazerak.UWP/GameObjects/Static/TextLabel.cs
@@ -0,0 +1,14 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Objects.Containers;
+
+using Microsoft.Xna.Framework;
+
+namespace JackSazerak.UWP.GameObjects.Static
+{
+    public class TextLabel : BaseTextGameObject
+    {
+        public TextLabel(string textStr, Color color, FONT_NAME fontName, HORIZONTAL_ALIGNMENT hAlignment, VERTICAL_ALIGNMENT vAlignment, GameWrapper wrapper, Vector2? position = null) : base(textStr, color, fontName, hAlignment, vAlignment, wrapper, position)
+        {  
+        }        
+    }
+}
