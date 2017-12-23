2f513701b5ff7333673c948cb27a0a45b80ba026
Jarred Capellman <jcapellman@hotmail.com>
2017-12-23 09:27:04 -0600

    Added: BaseMenuState to handle the common Menu State functionality such as Header and Backgrounds

---
+++ src/JackSazerak.UWP/States/BaseMenuState.cs
@@ -0,0 +1,44 @@
+﻿using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.GameObjects.Static;
+using JackSazerak.UWP.Objects.Containers;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace JackSazerak.UWP.States
+{
+    public abstract class BaseMenuState : BaseState
+    {
+        private TextLabel tlHeader;
+        private Background bMain;
+        
+        protected void SetHeader(string headerStr, GameWrapper gameWrapper)
+        {
+            if (!string.IsNullOrEmpty(headerStr))
+            {
+                tlHeader = new TextLabel(headerStr, Color.White, FONT_NAME.MAINMENU, HORIZONTAL_ALIGNMENT.CENTER, VERTICAL_ALIGNMENT.TOP, gameWrapper);
+            }
+        }
+
+        protected void SetBackground(string backgroundName, GameWrapper gameWrapper)
+        {
+            if (!string.IsNullOrEmpty(backgroundName))
+            {
+                bMain = new Background(backgroundName, gameWrapper);
+            }
+        }
+        
+        public override void Render(SpriteBatch spriteBatch)
+        {
+            bMain?.Render(spriteBatch);
+
+            tlHeader?.Render(spriteBatch);
+        }
+
+        public override void HandleInputs(KeyboardState state)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
