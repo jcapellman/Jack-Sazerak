158d8d94c6989063f9996f9db594b794e7ed71f8
Jarred Capellman <jcapellman@hotmail.com>
2017-12-23 11:46:17 -0600

    Added: Offset X and Y support to the TextLabel Class

--- src/JackSazerak.UWP/GameObjects/Static/TextLabel.cs
+++ src/JackSazerak.UWP/GameObjects/Static/TextLabel.cs
@@ -7,7 +7,7 @@ namespace JackSazerak.UWP.GameObjects.Static
 {
     public class TextLabel : BaseTextGameObject
     {
-        public TextLabel(string textStr, Color color, FONT_NAME fontName, HORIZONTAL_ALIGNMENT hAlignment, VERTICAL_ALIGNMENT vAlignment, GameWrapper wrapper, Vector2? position = null) : base(textStr, color, fontName, hAlignment, vAlignment, wrapper, position)
+        public TextLabel(string textStr, Color color, FONT_NAME fontName, HORIZONTAL_ALIGNMENT hAlignment, VERTICAL_ALIGNMENT vAlignment, GameWrapper wrapper, Vector2? position = null, float? offsetX = null, float? offsetY = null) : base(textStr, color, fontName, hAlignment, vAlignment, wrapper, position, offsetX, offsetY)
         {  
         }        
     }
