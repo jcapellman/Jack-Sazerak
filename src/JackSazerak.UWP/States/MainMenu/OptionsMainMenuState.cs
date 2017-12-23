cd212be82864df1328b67ea158b58fd23a589294
Jarred Capellman <jcapellman@hotmail.com>
2017-12-23 09:29:33 -0600

    Change: Refactored the OptionsMainMenuState to utilize the BaseMenuState class instead of the BaseState class

--- src/JackSazerak.UWP/States/MainMenu/OptionsMainMenuState.cs
+++ src/JackSazerak.UWP/States/MainMenu/OptionsMainMenuState.cs
@@ -1,28 +1,20 @@
 ﻿using JackSazerak.UWP.Enums;
-using JackSazerak.UWP.GameObjects.Static;
 using JackSazerak.UWP.Objects.Containers;
 
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace JackSazerak.UWP.States
+namespace JackSazerak.UWP.States.MainMenu
 {
-    public class OptionsMainMenuState : BaseState
+    public class OptionsMainMenuState : BaseMenuState
     {
-        private readonly Background background;
-
         public override GAME_STATES GameState => GAME_STATES.MAIN_MENU_OPTIONS;
 
         public OptionsMainMenuState(GameWrapper gameWrapper)
         {
-            background = new Background("options_menu", gameWrapper);
+            SetHeader("OPTIONS", gameWrapper);
+            SetBackground("options_menu", gameWrapper);            
         }
-
-        public override void Render(SpriteBatch spriteBatch)
-        {
-            background.Render(spriteBatch);
-        }
-
+        
         public override void HandleInputs(KeyboardState state)
         {
             if (state.IsKeyDown(Keys.Escape))
