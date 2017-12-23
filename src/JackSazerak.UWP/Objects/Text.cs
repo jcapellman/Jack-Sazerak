70433dbff80290efaf5e64f82bd46b950f922185
Jarred Capellman <jcapellman@hotmail.com>
2017-12-23 08:33:40 -0600

    Change: Revamped the Text Class to support dynamic positioning and Fonts

--- src/JackSazerak.UWP/Objects/Text.cs
+++ src/JackSazerak.UWP/Objects/Text.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using JackSazerak.UWP.Enums;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -9,19 +11,33 @@ namespace JackSazerak.UWP.Objects
         private readonly string textStr;
         private readonly Color color;
         private readonly SpriteFont spriteFont;
+        private Vector2 position;
 
-        public Text(string str, Color color, ContentManager contentManager)
+        public Text(string str, Color color, Vector2? position, FONT_NAME fontName, ContentManager contentManager)
         {
             textStr = str;
 
             this.color = color;
             
-            spriteFont = contentManager.Load<SpriteFont>($"Fonts/HUD");
+            spriteFont = contentManager.Load<SpriteFont>($"Fonts/{fontName}");
+
+            if (position.HasValue) { 
+                this.position = position.Value;
+            }
+        }
+
+        protected float Width => spriteFont.MeasureString(textStr).X;
+
+        protected float Height => spriteFont.MeasureString(textStr).Y;
+
+        protected void UpdatePosition(Vector2 position)
+        {
+            this.position = position;
         }
 
         public void Render(SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawString(spriteFont, textStr, new Vector2(100, 100), color);
+            spriteBatch.DrawString(spriteFont, textStr, position, color);
         }
     }
 }
