b8afbec9f9f73c052c540a15bd391dd0655cca87
Jarred Capellman <jcapellman@hotmail.com>
2017-12-24 09:19:21 -0600

    Added: Initial work on the GameSave object

---
+++ src/JackSazerak.UWP/Objects/JSONObjects/GameSave.cs
@@ -0,0 +1,11 @@
+﻿namespace JackSazerak.UWP.Objects.JSONObjects
+{
+    public class GameSave
+    {
+        public int Money { get; set; }
+
+        public int PlayerLevel { get; set; }
+
+        public int CurrentLevel { get; set; }
+    }
+}
