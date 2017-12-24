30c4e840cf202dcfdbe33e6021881bae26e18e35
Jarred Capellman <jcapellman@hotmail.com>
2017-12-24 09:18:26 -0600

    Added: Visual Indication of no save games found

--- src/JackSazerak.UWP/States/MainMenu/LoadGameMainMenuState.cs
+++ src/JackSazerak.UWP/States/MainMenu/LoadGameMainMenuState.cs
@@ -1,6 +1,12 @@
-﻿using JackSazerak.UWP.Enums;
-using JackSazerak.UWP.Objects.Containers;
+﻿using System.Collections.Generic;
+using System.Linq;
 
+using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.Managers;
+using JackSazerak.UWP.Objects.Containers;
+using JackSazerak.UWP.Objects.JSONObjects;
+
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace JackSazerak.UWP.States.MainMenu
@@ -9,10 +15,21 @@ namespace JackSazerak.UWP.States.MainMenu
     {
         public override GAME_STATES GameState => GAME_STATES.MAIN_MENU_LOADGAME;
 
+        private List<GameSave> gameSaves;
+
         public LoadGameMainMenuState(GameWrapper gameWrapper)
         {
             SetBackground("loadgame_menu", gameWrapper);
             SetHeader("LOAD GAME", gameWrapper);
+
+            gameSaves = GameSaveManager.GetSavedGames();
+
+            if (!gameSaves.Any())
+            {
+                AddTextLabel("No Save Games Found", Color.White, HORIZONTAL_ALIGNMENT.CENTER, VERTICAL_ALIGNMENT.CENTER, gameWrapper);
+
+                return;
+            }
         }
 
         public override void HandleInputs(KeyboardState state)
