4da930e068832b32f246c4b3ef92ecfdce3b95d1
Jarred Capellman <jcapellman@hotmail.com>
2017-12-24 08:55:12 -0600

    Added: Support to Add Game Objects to the Render path for MenuState classes

--- src/JackSazerak.UWP/States/BaseMenuState.cs
+++ src/JackSazerak.UWP/States/BaseMenuState.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 using JackSazerak.UWP.Enums;
+using JackSazerak.UWP.GameObjects;
 using JackSazerak.UWP.GameObjects.Static;
 using JackSazerak.UWP.Objects.Containers;
 
@@ -16,9 +18,23 @@ namespace JackSazerak.UWP.States
 
         private readonly List<TextLabel> textLabels;
 
+        private Dictionary<Guid, BaseGameObject> gameObjects;
+
         public BaseMenuState()
         {
             textLabels = new List<TextLabel>();
+            gameObjects = new Dictionary<Guid, BaseGameObject>();
+        }
+
+        protected BaseGameObject GetGameObject(Guid guid) => gameObjects[guid];
+
+        protected Guid AddGameObject(BaseGameObject gameObject)
+        {
+            var guid = Guid.NewGuid();
+
+            gameObjects.Add(guid, gameObject);
+
+            return guid;
         }
 
         protected void AddTextLabel(string str, Color color, HORIZONTAL_ALIGNMENT hAlignment,
@@ -46,6 +62,11 @@ namespace JackSazerak.UWP.States
         public override void Render(SpriteBatch spriteBatch)
         {
             bMain?.Render(spriteBatch);
+
+            foreach (var gameObject in gameObjects.Values)
+            {
+                gameObject.Render(spriteBatch);
+            }
 
             foreach (var textLabel in textLabels)
             {
