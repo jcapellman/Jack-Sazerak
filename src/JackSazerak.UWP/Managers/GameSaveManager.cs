367b9f78a070a46bf6c136ac631c20813a55fa99
Jarred Capellman <jcapellman@hotmail.com>
2017-12-27 09:28:07 -0600

    Added: Initial Save Game Loading and Writing Support

--- src/JackSazerak.UWP/Managers/GameSaveManager.cs
+++ src/JackSazerak.UWP/Managers/GameSaveManager.cs
@@ -1,16 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+using JackSazerak.UWP.Common;
 using JackSazerak.UWP.Objects.JSONObjects;
+
+using Newtonsoft.Json;
 
 namespace JackSazerak.UWP.Managers
 {
-    public static class GameSaveManager
+    public class GameSaveManager
     {
-        public static List<GameSave> GetSavedGames() => new List<GameSave>();   // TODO
-
-        public static void SaveGame(GameSave gameSave)
+        private Windows.Storage.StorageFolder Folder => Windows.Storage.ApplicationData.Current.LocalFolder;
+        
+        public async Task<List<GameSave>> GetSavedGamesAsync()
         {
-            // TODO
+            var files = await Folder.GetFilesAsync();
+
+            var saveGames = new List<GameSave>();
+
+            foreach (var save in files.Where(a => a.Name.EndsWith(Constants.FILE_SAVE_EXTENSION)))
+            {
+                var fileContent = await Windows.Storage.FileIO.ReadTextAsync(save);
+
+                saveGames.Add((GameSave)JsonConvert.DeserializeObject(fileContent));
+            }
+
+            return saveGames;
+        }
+
+        public async void SaveGameAsync(GameSave gameSave)
+        {
+            var file = await Folder.GetFileAsync($"{gameSave.SaveFileName}{Constants.FILE_SAVE_EXTENSION}");
+
+            await Windows.Storage.FileIO.WriteTextAsync(file, JsonConvert.SerializeObject(gameSave));
         }
     }
 }
