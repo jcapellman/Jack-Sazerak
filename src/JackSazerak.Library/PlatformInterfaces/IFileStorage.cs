b22713c6c714b761a7f6cc68a490fcd832faa1fe
Jarred Capellman <jcapellman@hotmail.com>
2017-12-29 08:11:57 -0800

    Added: Initial IFileStorage Interface for Platform Abstractions

---
+++ src/JackSazerak.Library/PlatformInterfaces/IFileStorage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace JackSazerak.Library.PlatformInterfaces
+{
+    public interface IFileStorage
+    {
+        Task<bool> FileExistsAsync(string fileName);
+
+        Task<string> ReadTextFileAsync(string fileName);
+
+        Task<List<string>> GetFilesAsync();
+
+        Task<bool> WriteTextFileAsync(string fileName, string content);
+    }
+}
