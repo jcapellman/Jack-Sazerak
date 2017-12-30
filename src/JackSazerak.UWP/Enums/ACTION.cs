d96f11b5392bb7fbbef75f17d0d8b8a49620c427
Jarred Capellman <jcapellman@hotmail.com>
2017-12-30 07:38:57 -0800

    Added: Error Types to the ACTION Enumeration

--- src/JackSazerak.UWP/Enums/ACTION.cs
+++ src/JackSazerak.UWP/Enums/ACTION.cs
@@ -5,6 +5,9 @@
         PLAYER_MOVE_RIGHT,
         PLAYER_MOVE_LEFT,
         PLAYER_MOVE_UP,
-        PLAYER_MOVE_DOWN
+        PLAYER_MOVE_DOWN,
+        ERROR_WARNING,
+        ERROR_CRITICAL,
+        ERROR_INFO
     }
 }
