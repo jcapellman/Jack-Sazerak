3d3f0c7d63f0a65e1713fffeac655a47b914edd8
Jarred Capellman <jcapellman@hotmail.com>
2017-12-30 07:39:26 -0800

    Added: Initial commit of the ErrorManager

---
+++ src/JackSazerak.UWP/Managers/ErrorManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using JackSazerak.Library.PlatformInterfaces;
+using JackSazerak.UWP.Objects.Containers;
+
+namespace JackSazerak.UWP.Managers
+{
+    public class ErrorManager
+    {
+        private IUserInterface userInterface;
+
+        public ErrorManager(GameWrapper gameWrapper)
+        {
+            userInterface = gameWrapper.UserInterface;
+
+            EventManager.EventOccurred += EventManager_EventOccurred;
+        }
+
+        private void EventManager_EventOccurred(object sender, (Enums.ACTION eventType, object argument) param)
+        {
+            // TODO Handle various levels to local storage, WebAPI upload and user notification
+            switch (param.eventType)
+            {
+                case Enums.ACTION.ERROR_CRITICAL:
+                    break;
+                case Enums.ACTION.ERROR_INFO:
+                    break;
+                case Enums.ACTION.ERROR_WARNING:
+                    break;
+                default:
+                    return;
+            }
+
+            userInterface.ShowMessageBox(Common.Constants.GAME_NAME, ((Exception)param.argument).ToString());
+        }
+    }
+}
