5d332027f2dd9f4ac8713a9d7f05331029b81fde
Jarred Capellman <jcapellman@hotmail.com>
2017-12-30 07:19:58 -0800

    Added: Initial implementation of the UserInterface Interface

---
+++ src/JackSazerak.UWP/PlatformImplementations/UserInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using JackSazerak.Library.PlatformInterfaces;
+
+using Windows.UI.Popups;
+
+namespace JackSazerak.UWP.PlatformImplementations
+{
+    public class UserInterface : IUserInterface
+    {
+        public async void ShowMessageBox(string title, string content)
+        {
+            var dialog = new MessageDialog(content);
+
+            await dialog.ShowAsync();
+        }
+    }
+}
