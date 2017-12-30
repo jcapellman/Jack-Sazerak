581395aa862bb054b5f401e64ca75e8d7ce621f8
Jarred Capellman <jcapellman@hotmail.com>
2017-12-29 20:09:09 -0800

    Added: ReturnWrapper container class

---
+++ src/JackSazerak.Library/Common/ReturnWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JackSazerak.Library.Common
+{
+    public class ReturnWrapper<T>
+    {
+        public T Object { get; private set; }
+
+        public Exception ReturnException { get; private set; }
+
+        public bool HasException { get { return ReturnException != null; } }
+
+        public ReturnWrapper(T objectValue)
+        {
+            Object = objectValue;
+        }
+
+        public ReturnWrapper(Exception exception)
+        {
+            ReturnException = exception;
+        }
+    }
+}
