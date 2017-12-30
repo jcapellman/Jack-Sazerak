c39d1e57e165a754d69b68d3b80cfcf70316089f
Jarred Capellman <jcapellman@hotmail.com>
2017-12-30 07:39:53 -0800

    Added: Optional argument support to the EventManager

--- src/JackSazerak.UWP/Managers/EventManager.cs
+++ src/JackSazerak.UWP/Managers/EventManager.cs
@@ -4,11 +4,11 @@ namespace JackSazerak.UWP.Managers
 {
     public static class EventManager
     {
-        public static event EventHandler<Enums.ACTION> EventOccurred;
+        public static event EventHandler<(Enums.ACTION, object)> EventOccurred;
         
         public static void FireEvent(Enums.ACTION actionTaken, object argument = null)
         {
-            EventOccurred?.Invoke(null, actionTaken);
+            EventOccurred?.Invoke(null, (actionTaken, argument));
         }
     }
 }
