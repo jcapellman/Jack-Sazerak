ac612921eee24ad8d4edabb4949dac05cc6b02d2
Jarred Capellman <jcapellman@hotmail.com>
2017-12-31 08:17:34 -0800

    Change: Cleaned up the Enumeration -> Description Helper Method

--- src/JackSazerak.UWP/Helpers/ExtensionMethods.cs
+++ src/JackSazerak.UWP/Helpers/ExtensionMethods.cs
@@ -1,28 +1,14 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 namespace JackSazerak.UWP
 {
     public static class ExtensionMethods
     {
-        public static string GetDescription<T>(this T enumerationValue) where T : struct
-        {
-            Type type = enumerationValue.GetType();
-
-            MemberInfo[] memberInfo = type.GetMember(enumerationValue.ToString());
-
-            if (memberInfo != null && memberInfo.Length > 0)
-            {
-                object[] attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
-
-                if (attrs != null && attrs.Length > 0)
-                {
-                    return ((DescriptionAttribute)attrs[0]).Description;
-                }
-            }
-
-            return enumerationValue.ToString();
-        }
+        public static string GetDescription<T>(this T enumerationValue) where T : struct => 
+            typeof(T).GetMember(enumerationValue.ToString())
+                .SelectMany(a => a.GetCustomAttributes<DescriptionAttribute>(false), (a, b) => b.Description)
+                .FirstOrDefault() ?? enumerationValue.ToString();
     }
 }
