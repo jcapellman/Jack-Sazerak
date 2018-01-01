8a1e16effd7748c64bfbabc476e7e7b97a38895e
Jarred Capellman <jcapellman@hotmail.com>
2018-01-01 07:00:33 -0800

    Added: Type and Message logic in the JackException class

--- src/JackSazerak.Library/Common/JackException.cs
+++ src/JackSazerak.Library/Common/JackException.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using JackSazerak.Library.Enums;
+using System;
 
 namespace JackSazerak.Library.Common
 {
     public class JackException : Exception
     {
+        public ExceptionTypes ExceptionType { get; set; }
+
         public string ExceptionTitle { get; set; }
 
         public string ExceptionMessage { get; set; }
+        
+        public JackException(ExceptionTypes exceptionType, Exception exception, string userExceptionMessage = null)
+        {
+            ExceptionType = exceptionType;
+
+            ExceptionTitle = ExceptionType.GetDescription();
+
+            if (string.IsNullOrEmpty(userExceptionMessage))
+            {
+                ExceptionMessage = exception.ToString();
+            } else
+            {
+                ExceptionMessage = userExceptionMessage;
+            }
+        }
     }
 }
