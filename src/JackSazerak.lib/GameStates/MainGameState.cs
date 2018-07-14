443ff644a51b05865962f350d2acbc9d794ec81d
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:46:17 -0500

    Added: Initial work on the MainGameState class

---
+++ src/JackSazerak.lib/GameStates/MainGameState.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+using JackSazerak.lib.GameStates.Base;
+using JackSazerak.lib.RenderableObjects;
+
+namespace JackSazerak.lib.GameStates
+{
+    public class MainGameState : BaseState
+    {
+        public MainGameState()
+        {
+            AddObject(new TextObject("Jack Sazerak", 100, 100, Color.Black));
+        }
+    }
+}
