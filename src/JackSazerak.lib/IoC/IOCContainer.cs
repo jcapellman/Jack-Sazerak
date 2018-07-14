3ad7e8f5b92182c95a9cfd3f6b40e8baa5563505
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:46:33 -0500

    Added: Initial work on the IOCContainer class

---
+++ src/JackSazerak.lib/IoC/IOCContainer.cs
@@ -0,0 +1,18 @@
+﻿using JackSazerak.lib.BaseImplementations;
+
+using Ninject;
+
+namespace JackSazerak.lib.IoC
+{
+    public static class IOCContainer
+    {
+        private static IKernel Kernel { get; set; } = new StandardKernel();
+
+        public static BaseGraphicsRenderer GfxRenderer => Kernel.Get<BaseGraphicsRenderer>();
+
+        public static void Initialize(BaseGraphicsRenderer graphicsRenderer)
+        {
+            Kernel.Bind<BaseGraphicsRenderer>().To(graphicsRenderer.GetType()).InSingletonScope();            
+        }
+    }
+}
