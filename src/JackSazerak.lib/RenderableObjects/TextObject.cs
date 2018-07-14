5fc476566972232c5257677b284470a7f2529616
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:45:45 -0500

    Added: Initial work on the TextObject class

---
+++ src/JackSazerak.lib/RenderableObjects/TextObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+using JackSazerak.lib.IoC;
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.RenderableObjects
+{
+    public class TextObject : BaseRenderableObject
+    {
+        private string _text;
+        private float _xPosition;
+        private float _yPosition;
+        private Color _color;
+
+        public TextObject(string text, float xPosition, float yPosition, Color color)
+        {
+            _text = text;
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+            _color = color;
+        }
+
+        public override void Render()
+        {
+            IOCContainer.GfxRenderer.DrawText(_text, _xPosition, _yPosition, _color);
+        }
+    }
+}
