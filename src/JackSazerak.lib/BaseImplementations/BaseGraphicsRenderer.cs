18dc1d8b018d18f7c16a925a5ebef7e1565fe394
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:45:10 -0500

    Added: Initial work on the BaseGraphicsRenderer

---
+++ src/JackSazerak.lib/BaseImplementations/BaseGraphicsRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.BaseImplementations
+{
+    public abstract class BaseGraphicsRenderer
+    {
+        public abstract void DrawText(string text, float xPosition, float yPosition, Color color);
+
+        public virtual void Render(object renderObject, List<BaseRenderableObject> renderables)
+        {
+            foreach (var renderable in renderables)
+            {
+                renderable.Render();
+            }
+        }
+    }
+}
