c37eb9c918e6c9a734f3150928426523db007d00
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:44:51 -0500

    Added: Initial work on the MainPage class of the UWP project

---
+++ src/JackSazerak.UWP/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using JackSazerak.lib.GameStates;
+using JackSazerak.lib.GameStates.Base;
+
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace JackSazerak.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+        private BaseState _currentState;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _currentState = new MainGameState();
+        }
+
+        void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            _currentState.Render(args.DrawingSession);
+        }
+    }
+}
