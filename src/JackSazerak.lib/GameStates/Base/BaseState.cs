adeea819fc81ff41670df1a3f463209895c18301
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:45:27 -0500

    Added: Initial work on the BaseState

---
+++ src/JackSazerak.lib/GameStates/Base/BaseState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using JackSazerak.lib.IoC;
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.GameStates.Base
+{
+    public abstract class BaseState
+    {
+        private readonly List<BaseRenderableObject> _renderables;
+
+        protected BaseState()
+        {
+            _renderables = new List<BaseRenderableObject>();
+        }
+
+        protected void AddObject(BaseRenderableObject renderableObject)
+        {
+            _renderables.Add(renderableObject);
+        }
+
+        public void Render(object renderObject)
+        {
+            IOCContainer.GfxRenderer.Render(renderObject, _renderables);
+        }
+    }
+}
