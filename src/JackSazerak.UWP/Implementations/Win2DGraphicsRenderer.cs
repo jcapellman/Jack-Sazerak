3ed138b0fc0c59aa704f5b0711232ef53237e600
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 13:44:25 -0500

    Added: Initial work on the Win2D Renderer

---
+++ src/JackSazerak.UWP/Implementations/Win2DGraphicsRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+using JackSazerak.lib.BaseImplementations;
+using JackSazerak.lib.RenderableObjects.Base;
+
+using Microsoft.Graphics.Canvas;
+
+namespace JackSazerak.UWP.Implementations
+{
+    public class Win2DGraphicsRenderer : BaseGraphicsRenderer
+    {
+        private CanvasDrawingSession _session;
+
+        public  override void DrawText(string text, float xPosition, float yPosition, Color color)
+        {
+            _session.DrawText(text, xPosition, yPosition, Windows.UI.Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        public override void Render(object renderObject, List<BaseRenderableObject> renderables)
+        {
+            _session = (CanvasDrawingSession) renderObject;
+
+            base.Render(null, renderables);
+        }
+    }
+}
