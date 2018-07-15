92eda10760404a3b13a7ea22bbf8212677fe622a
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 14:12:21 -0500

    Added: Initial work on the StaticTile class

---
+++ src/JackSazerak.lib/GameObjects/Tile/StaticTile.cs
@@ -0,0 +1,16 @@
+﻿using JackSazerak.lib.GameObjects.Tile.Base;
+
+namespace JackSazerak.lib.GameObjects.Tile
+{
+    public class StaticTile : BaseTile
+    {
+        public StaticTile(string spriteFileName, float xRow, float yRow) : base(spriteFileName, xRow, yRow)
+        {
+        }
+
+        public override void Render()
+        {
+            RenderObject.Render();
+        }
+    }
+}
