c47db8702b413cc03023698363b5a504a531b41a
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 19:40:34 -0500

    Partial commit for Resource Manager abstraction

---
+++ src/JackSazerak.UWP/Implementations/UWPResourceManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Graphics.Canvas;
+
+using JackSazerak.lib.BaseImplementations;
+
+namespace JackSazerak.UWP.Implementations
+{
+    public class UWPResourceManager : BaseResourceManager
+    {
+        public class Texture
+        {
+            public byte[] arr;
+        }
+
+        public enum ResourceType
+        {
+            SPRITE,
+            TEXTURE,
+            SOUND
+        }
+
+        public async Task<Texture> LoadTextureAsync(string textureName, ResourceType resourceType)
+        {
+            var result = await CanvasBitmap.LoadAsync(null, new Uri($"ms-appx:///Assets/{resourceType}/{textureName}"));
+            
+            return new Texture();
+        } 
+    }
+}
---
+++ src/JackSazerak.lib/BaseImplementations/BaseResourceManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JackSazerak.lib.BaseImplementations
+{
+    public abstract class BaseResourceManager
+    {
+
+    }
+}
