e6750e77875975479ab0dda3aecb965900bec46f
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 10:30:32 -0500

    Added: Initial work on the PlayerObject class

---
+++ src/JackSazerak.lib/GameObjects/PlayerObject.cs
@@ -0,0 +1,22 @@
+﻿using JackSazerak.lib.GameObjects.Aircraft.Base;
+using JackSazerak.lib.GameObjects.Base;
+
+namespace JackSazerak.lib.GameObjects
+{
+    public class PlayerObject : BaseGameObject
+    {
+        private BaseAircraft _playerAircraft;
+
+        public PlayerObject(BaseAircraft aircraft)
+        {
+            _playerAircraft = aircraft;
+
+            RenderObject = _playerAircraft.RenderObject;
+        }
+
+        public override void Render()
+        {
+            _playerAircraft.Render();
+        }
+    }
+}
