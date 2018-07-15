e6edd07dcb4c08186b5af17f9a006377603cb51c
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 23:06:28 -0500

    Added: Win2D work around for loading resources on the fly

--- src/JackSazerak.UWP/MainPage.xaml.cs
+++ src/JackSazerak.UWP/MainPage.xaml.cs
@@ -1,60 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using Windows.UI.Xaml.Controls;
 
 using JackSazerak.lib.GameStates;
 using JackSazerak.lib.GameStates.Base;
-using JackSazerak.lib.IoC;
-using JackSazerak.UWP.Implementations;
 
 using Microsoft.Graphics.Canvas.UI.Xaml;
 using Microsoft.Graphics.Canvas;
-using System.Threading.Tasks;
 
 namespace JackSazerak.UWP
 {
     public sealed partial class MainPage : Page
     {
         private BaseState _currentState;
+        private BaseState _nextState = null;
 
-        private UWPResourceManager _resourceManager = new UWPResourceManager();
-
-        private Queue<string> _resourceQueue;
+        Task stateLoadTask;
 
         public MainPage()
         {
             InitializeComponent();
-            
-            _resourceQueue = new Queue<string>();
-            _resourceManager.OnTextureLoadRequest += _resourceManager_OnTextureLoadRequest;
 
-            IOCContainer.Initialize(new Win2DGraphicsRenderer(_resourceManager));
+            _nextState = new MainGameState();
+        }
+        
+        async Task LoadResourcesForStateAsync(CanvasControl resourceCreator, BaseState state)
+        {
+            foreach (var item in state.Renderables)
+            {
+                item.Resource = await CanvasBitmap.LoadAsync(resourceCreator, item.ResouceFileName);
+            }
             
-            _currentState = new MainGameState();
+            _currentState = state;
         }
 
-        private void _resourceManager_OnTextureLoadRequest(object sender, string e)
+        public void LoadState(BaseState state)
         {
-           var result = CanvasBitmap.LoadAsync(cControl, e);
+            _nextState = state;
 
-            var temp = result;
+            stateLoadTask = LoadResourcesForStateAsync(cControl, _nextState);
         }
 
         void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
         {
-            _currentState.Render(args.DrawingSession);
+            _currentState?.Render(args.DrawingSession);
         }
 
         async Task CreateResourcesAsync(CanvasControl sender)
         {
-            for (var x = 0; x < _resourceQueue.Count; x++) {
-                var textureFileName = _resourceQueue.Dequeue();
+            if (stateLoadTask != null)
+            {
+                stateLoadTask.AsAsyncAction().Cancel();
 
-                var result = await CanvasBitmap.LoadAsync(sender, textureFileName);
+                try { await stateLoadTask; } catch { }
+
+                stateLoadTask = null;
+            }
+
+            if (_nextState != null)
+            {
+                LoadState(_nextState);
             }
         }
-
+        
         private void cControl_CreateResources(CanvasControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
         {
             args.TrackAsyncAction(CreateResourcesAsync(sender).AsAsyncAction());
