001688a52ad78a7646910869829240c42c9b37da
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 23:07:35 -0500

    Change: Removed LoadTexture method in the BaseGraphicsRenderer

--- src/JackSazerak.lib/BaseImplementations/BaseGraphicsRenderer.cs
+++ src/JackSazerak.lib/BaseImplementations/BaseGraphicsRenderer.cs
@@ -9,9 +9,7 @@ namespace JackSazerak.lib.BaseImplementations
     public abstract class BaseGraphicsRenderer
     {
         public abstract void DrawText(string text, float xPosition, float yPosition, Color color);
-
-        public abstract void LoadTexture(string textureName, ResourceTypes resourceType);
-
+        
         public abstract void DrawTexture(object textureObject, float xPosition, float yPosition, float scale);
 
         public virtual void Render(object renderObject, List<BaseRenderableObject> renderables)
