356d8b3f7ba0446477a96df5ba89a8bfd8e4e988
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 14:12:37 -0500

    Added: Initial work on the MapObject Class

---
+++ src/JackSazerak.lib/RenderableObjects/MapObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+using JackSazerak.lib.Enums;
+using JackSazerak.lib.GameObjects.Tile;
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.RenderableObjects
+{
+    public class MapObject : BaseRenderableObject
+    {
+        private List<StaticTile> _tiles;
+
+        public MapObject(List<StaticTile> tiles)
+        {
+            _tiles = tiles;
+
+            ResourceObject = new Objects.ResourceObject("Water.jpg", ResourceTypes.TILE);
+        }
+
+        public override void Render()
+        {
+            foreach (var tile in _tiles)
+            {
+                if (tile.RenderObject?.Resource == null)
+                {
+                    tile.RenderObject.Resource = ResourceObject.Resource;
+                }
+
+                tile.Render();
+            }
+        }
+    }
+}
