c7e3b025df7af88f680259e736ef57234d10b843
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 10:29:58 -0500

    Added: Initial work on the BaseAircraft class

---
+++ src/JackSazerak.lib/GameObjects/Aircraft/Base/BaseAircraft.cs
@@ -0,0 +1,13 @@
+﻿using JackSazerak.lib.GameObjects.Base;
+using JackSazerak.lib.RenderableObjects;
+
+namespace JackSazerak.lib.GameObjects.Aircraft.Base
+{
+    public abstract class BaseAircraft : BaseGameObject
+    {
+        protected BaseAircraft(string spriteFileName)
+        {
+            RenderObject = new SpriteObject(spriteFileName, 0, 0, 1.0f);
+        }
+    }
+}
