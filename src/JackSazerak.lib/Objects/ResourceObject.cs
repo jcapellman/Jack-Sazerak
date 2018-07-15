2a5f657c43b22fbdd343c7bdeef9efd0a287893d
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 23:04:08 -0500

    Added: Initial work on the ResourceObject class

---
+++ src/JackSazerak.lib/Objects/ResourceObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using JackSazerak.lib.Enums;
+
+namespace JackSazerak.lib.Objects
+{
+    public class ResourceObject
+    {
+        public Guid GUID { get; private set; }
+
+        public ResourceTypes ResourceType { get; set; }
+
+        public Object Resource { get; set; }
+
+        public string FileName { get; set; }
+
+        public ResourceObject(string fileName, ResourceTypes resourceType)
+        {
+            GUID = Guid.NewGuid();
+
+            FileName = fileName;
+            ResourceType = resourceType;
+        }
+    }
+}
