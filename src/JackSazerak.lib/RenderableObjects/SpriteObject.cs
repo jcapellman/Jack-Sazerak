23f59a38af6566e1e7c13fd59209b1c6de962686
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 10:32:45 -0500

    Added: Position Updating support in the SpriteObject

--- src/JackSazerak.lib/RenderableObjects/SpriteObject.cs
+++ src/JackSazerak.lib/RenderableObjects/SpriteObject.cs
@@ -1,26 +1,25 @@
 ﻿using JackSazerak.lib.Enums;
+using JackSazerak.lib.Objects;
 using JackSazerak.lib.RenderableObjects.Base;
 
 namespace JackSazerak.lib.RenderableObjects
 {
     public class SpriteObject : BaseRenderableObject
     {
-        private float _xPosition;
-        private float _yPosition;
         private float _scale;
 
         public SpriteObject(string resourceFileName, float xPosition, float yPosition, float scale)
         {
-            _xPosition = xPosition;
-            _yPosition = yPosition;
+            UpdatePosition(new Point(xPosition, yPosition));
+
             _scale = scale;
 
-            _resourceObject = new Objects.ResourceObject(resourceFileName, ResourceTypes.SPRITE);
+            ResourceObject = new Objects.ResourceObject(resourceFileName, ResourceTypes.SPRITE);
         }
         
         public override void Render()
         {
-            IoC.IOCContainer.GfxRenderer.DrawTexture(_resourceObject.Resource, _xPosition, _yPosition, _scale);
+            IoC.IOCContainer.GfxRenderer.DrawTexture(ResourceObject.Resource, _currentPosition.X, _currentPosition.Y, _scale);
         }
     }
 }
