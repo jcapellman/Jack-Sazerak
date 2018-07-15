e03ec0a618f99f3f35c9d142f20ac9554ebffb06
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 10:30:20 -0500

    Added: Initial work on the BaseGameObject class

---
+++ src/JackSazerak.lib/GameObjects/Base/BaseGameObject.cs
@@ -0,0 +1,21 @@
+﻿using JackSazerak.lib.Objects;
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.GameObjects.Base
+{
+    public abstract class BaseGameObject
+    {
+        protected Point _currentPosition;
+
+        public BaseRenderableObject RenderObject;
+
+        public void UpdatePosition(Point point)
+        {
+            _currentPosition = point;
+
+            RenderObject.UpdatePosition(point);
+        }
+
+        public abstract void Render();
+    }
+}
