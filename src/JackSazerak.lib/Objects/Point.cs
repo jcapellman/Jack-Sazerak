88de00fd34d755aa7f9809b0fa17be0afc5793d1
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 10:31:01 -0500

    Added: Point Class

---
+++ src/JackSazerak.lib/Objects/Point.cs
@@ -0,0 +1,15 @@
+﻿namespace JackSazerak.lib.Objects
+{
+    public class Point
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
