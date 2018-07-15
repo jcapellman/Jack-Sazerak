f72d01753b290908f60ad8f87a9aaf7b04a36e8e
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 14:12:06 -0500

    Added: Initial work on the StaticTile class

---
+++ src/JackSazerak.lib/GameObjects/Tile/Base/BaseTile.cs
@@ -0,0 +1,13 @@
+﻿using JackSazerak.lib.GameObjects.Base;
+using JackSazerak.lib.RenderableObjects;
+
+namespace JackSazerak.lib.GameObjects.Tile.Base
+{
+    public abstract class BaseTile : BaseGameObject
+    {
+        protected BaseTile(string spriteFileName, float xRow, float yRow)
+        {
+            RenderObject = new TileObject(spriteFileName, xRow * 512, yRow * 512, 1.0f);
+        }
+    }
+}
