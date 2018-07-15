91754e1077cc272eec5de343cda22893fd7775f3
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 12:36:08 -0500

    Added: Window Collision Buffer Constants

--- src/JackSazerak.lib/Common/Constants.cs
+++ src/JackSazerak.lib/Common/Constants.cs
@@ -4,5 +4,8 @@
     {
         public const float DESIGN_WIDTH = 1920;
         public const float DESIGN_HEIGHT = 1080;
+
+        public const float WINDOW_WIDTH_COLLISION_BUFFER = 35;
+        public const float WINDOW_HEIGHT_COLLISION_BUFFER = 25;    
     }
 }
