54d4db7a882d0ed88eae87afebeb5775fd4f136e
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 23:09:29 -0500

    Added: Resource Object to the BaseRenderableObject class

--- src/JackSazerak.lib/RenderableObjects/Base/BaseRenderableObject.cs
+++ src/JackSazerak.lib/RenderableObjects/Base/BaseRenderableObject.cs
@@ -1,7 +1,28 @@
-﻿namespace JackSazerak.lib.RenderableObjects.Base
+﻿using JackSazerak.lib.Enums;
+using JackSazerak.lib.Objects;
+
+namespace JackSazerak.lib.RenderableObjects.Base
 {
     public abstract class BaseRenderableObject
     {
         public abstract void Render();
+
+        protected ResourceObject _resourceObject;
+
+        public string ResouceFileName => _resourceObject?.FileName;
+
+        public ResourceTypes ResourceType => _resourceObject.ResourceType;
+
+        public object Resource {
+            get
+            {
+                return _resourceObject.Resource;
+            }
+
+            set
+            {
+                _resourceObject.Resource = value;
+            }
+        }
     }
 }
