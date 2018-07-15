c0d92e4121e38554829e612a2f2e37b6fcd43428
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 14:13:29 -0500

    Added: Initial work on the TileObject class

---
+++ src/JackSazerak.lib/RenderableObjects/TileObject.cs
@@ -0,0 +1,25 @@
+﻿using JackSazerak.lib.Enums;
+using JackSazerak.lib.Objects;
+using JackSazerak.lib.RenderableObjects.Base;
+
+namespace JackSazerak.lib.RenderableObjects
+{
+    public class TileObject : BaseRenderableObject
+    {
+        private readonly float _scale;
+
+        public TileObject(string resourceFileName, float xPosition, float yPosition, float scale)
+        {
+            UpdatePosition(new Point(xPosition, yPosition));
+
+            _scale = scale;
+
+            ResourceObject = new ResourceObject(resourceFileName, ResourceTypes.TILE);
+        }
+
+        public override void Render()
+        {
+            IoC.IOCContainer.GfxRenderer.DrawTexture(ResourceObject.Resource, _currentPosition.X, _currentPosition.Y, _scale);
+        }
+    }
+}
