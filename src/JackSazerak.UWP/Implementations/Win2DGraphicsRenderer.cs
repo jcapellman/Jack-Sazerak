9c9b7a2d1e092327e1cc8841ed79a7599265bd32
Jarred Capellman <jcapellman@hotmail.com>
2018-07-14 23:05:28 -0500

    Change: Remove LoadTexture and Resource related code

--- src/JackSazerak.UWP/Implementations/Win2DGraphicsRenderer.cs
+++ src/JackSazerak.UWP/Implementations/Win2DGraphicsRenderer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Threading.Tasks;
 
 using JackSazerak.lib.BaseImplementations;
 using JackSazerak.lib.Enums;
@@ -14,13 +13,6 @@ namespace JackSazerak.UWP.Implementations
     public class Win2DGraphicsRenderer : BaseGraphicsRenderer
     {
         private CanvasDrawingSession _session;
-
-        private UWPResourceManager _resourceManager;
-
-        public Win2DGraphicsRenderer(UWPResourceManager resourceManager)
-        {
-            _resourceManager = resourceManager;
-        }
         
         public override void DrawText(string text, float xPosition, float yPosition, Color color)
         {
@@ -33,19 +25,7 @@ namespace JackSazerak.UWP.Implementations
 
             base.Render(null, renderables);
         }
-
-        public override void LoadTexture(string textureName, ResourceTypes resourceType)
-        {
-            try
-            {
-                 _resourceManager.LoadTexture(textureName, resourceType);
-
-            } catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
+        
         public override void DrawTexture(object textureObject, float xPosition, float yPosition, float scale)
         {
             _session.DrawImage((CanvasBitmap)textureObject, xPosition, yPosition);
