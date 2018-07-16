f346532a354069b13f33b54f20d5256c706c451a
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 20:19:16 -0500

    Added: Initial work on the BaseViewModel class

---
+++ src/JackSazerak.Editor/ViewModels/BaseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace JackSazerak.Editor.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        #region MVVM Boilerplate code
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
