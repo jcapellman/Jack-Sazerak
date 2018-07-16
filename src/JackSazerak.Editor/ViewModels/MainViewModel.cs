436bd246e86729ac55b312d02e4f0a9975203526
Jarred Capellman <jcapellman@hotmail.com>
2018-07-15 20:19:47 -0500

    Added: Initial work on the MainViewModel Class

---
+++ src/JackSazerak.Editor/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using JackSazerak.Editor.Common;
+
+using System.Windows.Input;
+
+namespace JackSazerak.Editor.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private ICommand exitMenuCommand;
+
+        public ICommand ExitMenuCommand => exitMenuCommand ?? (exitMenuCommand = new CommandHandler(ExitEditor));
+
+        public void ExitEditor()
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
