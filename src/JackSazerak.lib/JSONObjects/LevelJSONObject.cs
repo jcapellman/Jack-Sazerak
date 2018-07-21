1aa836c8fed0308aa6b0dfe031478b2bbe7e7b98
Jarred Capellman <jcapellman@hotmail.com>
2018-07-20 21:45:28 -0500

    Added: LevelJSONObject class for storing the Level

---
+++ src/JackSazerak.lib/JSONObjects/LevelJSONObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace JackSazerak.lib.JSONObjects
+{
+    [DataContract]
+    public class LevelJSONObject
+    {
+        [DataMember]
+        public string FileName { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public List<string> Tiles { get; set; }
+    }
+}
