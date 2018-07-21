08db4264017f87006fac4aa5f0c0f995e285f4dd
Jarred Capellman <jcapellman@hotmail.com>
2018-07-21 12:11:26 -0500

    Added: TileExtensions Extension Method Class

---
+++ src/JackSazerak.Editor/Extensions/TileExtensions.cs
@@ -0,0 +1,16 @@
+﻿using JackSazerak.Editor.Objects;
+using JackSazerak.lib.JSONObjects;
+
+namespace JackSazerak.Editor.Extensions
+{
+    public static class TileExtensions
+    {
+        public static TileJSONObject TileJsonObject(this Tile tile) => new TileJSONObject
+        {
+            TextureName = tile.TextureName,
+            YPosition = tile.YPosition,
+            XPosition = tile.XPosition,
+            Layer = tile.Layer
+        };
+    }
+}
